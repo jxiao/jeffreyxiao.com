0f13f979378a4d93b192098d8ecf6fec89f14282
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2020-08-08 02:36:06 -0400

    refactoring html and js, fixed height issue of carousel

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,32 @@
-// Skills Section (Folder Structure)
+// Animated Scroll
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
 
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+        
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        } // End if
+    });
+});
+
+
+
+// Skills Section (Folder Structure)
 document.getElementById("hierarchyGeneral").addEventListener("click", hideShowEvent);
 document.getElementById("hierarchyWeb").addEventListener("click", hideShowEvent);
 document.getElementById("hierarchyDatabase").addEventListener("click", hideShowEvent);
@@ -25,6 +52,9 @@ function hideShowEvent(event){
   }
 }
 
+
+
+// Light-Dark Theme
 var checkbox = document.querySelector('input[name=theme]');
 checkbox.addEventListener('change', function() {
     if (this.checked) {
@@ -41,3 +71,31 @@ let trans = () => {
         document.documentElement.classList.remove('transition')
     }, 1000)
 }
+
+
+// OnloadEvents()
+function onloadEvents() {
+    // AOS Scroll Animation
+    AOS.init();
+    
+    // Carousel Height
+    setCarouselHeight();
+}
+
+
+// Carousel Height
+function setCarouselHeight() {
+    var pennHeight = document.getElementById('pennInfo').clientHeight;
+    document.getElementById("carouselExampleIndicators").style.maxHeight = `${pennHeight}px`;
+
+    var lst = document.getElementsByClassName("carousel-img");
+    for (var i = 0; i < lst.length; i++) {
+        lst[i].style.height = pennHeight+"px";
+    }
+}
+
+// Responsive resizing of carousel
+$(window).resize(function(){
+    winWidth = $(window).width();
+    setCarouselHeight();
+});
