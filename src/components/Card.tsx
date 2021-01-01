2e718d9233be065e2a3dda4a1698f34ee3e19cf6
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2021-01-01 00:28:29 -0500

    disable scroll when modal is shown

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { FluidObject } from "gatsby-image"
 import Img from "gatsby-image"
 import styled from "styled-components"
@@ -92,6 +92,19 @@ const Card = ({
   const openModal = () => {
     setShowModal(prev => !prev)
   }
+
+  useEffect(() => {
+    if (showModal) {
+      const scrollY = window.scrollY
+      document.body.style.position = "fixed"
+      document.body.style.top = `-${scrollY}px`
+    } else {
+      const scrollY = document.body.style.top
+      document.body.style.position = ""
+      document.body.style.top = ""
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1)
+    }
+  }, [showModal])
 
   return (
     <CardContainer>
