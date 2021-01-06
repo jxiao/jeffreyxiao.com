6a0716910d693097cdb151f38b2a740de677cd0b
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2021-01-06 13:06:03 -0500

    add helmet for light/dark mode (body)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,25 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import Home from "../components/Home"
+import ThemeContext from "../context/ThemeContext"
+import Helmet from "react-helmet"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title={"Jeffrey Xiao"} />
-    <Home />
-  </Layout>
+  <ThemeContext.Consumer>
+    {theme => (
+      <>
+        <Helmet
+          bodyAttributes={{
+            class: theme.dark ? "dark" : "light",
+          }}
+        />
+        <Layout>
+          <SEO title={"Jeffrey Xiao"} />
+          <Home />
+        </Layout>
+      </>
+    )}
+  </ThemeContext.Consumer>
 )
 
 export default IndexPage
