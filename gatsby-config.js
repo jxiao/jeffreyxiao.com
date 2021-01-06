6531bf63c1b62ac4eb486bf8dfc92cd2f6d1881b
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2021-01-06 13:54:56 -0500

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,17 @@ module.exports = {
     siteUrl: "https://www.jeffreyxiao.com",
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-186690354-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
